4040468da12893ba2d400226e7e0054151ca4f25
tylerwatson98 <32936743+tylerwatson98@users.noreply.github.com>
2018-10-12 11:24:29 -0600

    Update LonelyTwitterActivity.java

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -1,13 +1,11 @@
 package ca.ualberta.cs.lonelytwitter;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
@@ -15,8 +13,10 @@ import java.util.Date;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -27,113 +27,64 @@ import com.google.gson.reflect.TypeToken;
 
 public class LonelyTwitterActivity extends Activity {
 
-	// if something is static that means it extends across the entire program
-	// if something is final that means it is constant, you don't want to change it
 	private static final String FILENAME = "file.sav";
 	private EditText bodyText;
 	private ListView oldTweetsList;
-	private ArrayList<Tweet> tweets = new ArrayList<Tweet>();
+	private ArrayList<Tweet> tweetList = new ArrayList<Tweet>();
 	private ArrayAdapter<Tweet> adapter;
 
-	
-	/** Called when the activity is first created. */
+
+
 	@Override
-	// onCreate sets the layout of everything for the views so it doesn't have to be done later
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		bodyText = findViewById(R.id.body);
-		Button saveButton = findViewById(R.id.save);
-		Button clearButton = findViewById(R.id.clear);
-		oldTweetsList = findViewById(R.id.oldTweetsList);
+
+		bodyText = (EditText) findViewById(R.id.body);
+		Button saveButton = (Button) findViewById(R.id.save);
+		Button clearButton = (Button) findViewById(R.id.clear);
+		oldTweetsList = (ListView) findViewById(R.id.oldTweetsList);
 
 		saveButton.setOnClickListener(new View.OnClickListener() {
 
 			public void onClick(View v) {
 				setResult(RESULT_OK);
 				String text = bodyText.getText().toString();
-				Tweet newTweet= new ImportantTweet();
-				tweets.add(newTweet);
+				Tweet newTweet = new NormalTweet(text);
+				tweetList.add(newTweet);
 				adapter.notifyDataSetChanged();
-				new ElasticSearchTweetController.AddTweetTask().execute(newTweet);
+				new ElasticsearchTweetController
+						.AddTweetTask().execute(newTweet);
 
-/*
-				// set message of the new tweet
-				try {
-					newTweet.setMessage(text);
-					newTweet.setDate(new Date());
 
-					tweets.add(newTweet);
-					adapter.notifyDataSetChanged();
-					saveInFile();
-
-				} catch (MaxTextLength maxTextLength) {
-					maxTextLength.printStackTrace();
-				}
 			}
 		});
-
 
 		clearButton.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
 
-				tweets.clear();
+			public void onClick(View v) {
+				setResult(RESULT_OK);
+				tweetList.clear();
+				deleteFile(FILENAME);  // TODO deprecate this button
 				adapter.notifyDataSetChanged();
-				saveInFile();*/
 			}
 		});
+
+
 	}
 
-
-	// when you override a method the first thing you should write is super."method"
-
 	@Override
-	protected void onStart(){
+	protected void onStart() {
 		// TODO Auto-generated method stub
 		super.onStart();
-		loadFromFile();
-		adapter = new ArrayAdapter<Tweet>(this, R.layout.list_item, tweets);
-		// once the adapter is set the list will be displayed in that view
+		try {
+            tweetList = new ElasticsearchTweetController.GetTweetsTask().execute("").get();
+        }catch(Exception e){}// this is bad.. don't do this
+		adapter = new ArrayAdapter<Tweet>(this,
+				R.layout.list_item, tweetList);
 		oldTweetsList.setAdapter(adapter);
 	}
 
-	private void loadFromFile() {
-		try {
-			FileInputStream fis = openFileInput(FILENAME);
-			InputStreamReader isr = new InputStreamReader(fis);
-			BufferedReader reader = new BufferedReader(isr);
-			Gson gson = new Gson();
-			Type listTweetType = new TypeToken<ArrayList<ImportantTweet>>(){}.getType();
-			gson.fromJson(reader,listTweetType);
 
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			tweets=new ArrayList<Tweet>();
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+
 	}
-	
-	private void saveInFile() {
-		try {
-
-			// saving a file with gson
-			FileOutputStream fos = openFileOutput(FILENAME,0);
-			OutputStreamWriter osw = new OutputStreamWriter(fos);
-			BufferedWriter writer = new BufferedWriter(osw);
-			Gson gson = new Gson();
-			gson.toJson(tweets,writer);
-			writer.flush();
-			fos.close();
-
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-}
